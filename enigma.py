7df1e6d0f5f7c46493a35788f8cc3f3bafcaf90c
Ranieri Althoff <ranisalt@gmail.com>
2015-02-23 09:15:11 -0300

    Create the classes I will need

---
+++ enigma.py
@@ -0,0 +1,16 @@
+class Steckerbrett:
+    def __init__(self):
+        pass
+
+
+class Walzen:
+    def __init__(self):
+        pass
+
+
+class Enigma:
+    def __init__(self):
+        pass
+
+    def cipher(self, message):
+        pass
