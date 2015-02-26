147ab3511038ca938af7eaaac498afabfcaba36d
Ranieri Althoff <ranisalt@gmail.com>
2015-02-26 01:51:51 -0300

    Set up test with settings from Wikipedia

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 import unittest
 
+from enigma import Enigma, Umkehrwalze, Walzen
+
 
 class EnigmaTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        # Rotors go from right to left, so I reverse the tuple to make Rotor
+        # I be the leftmost. I may change this behavior in the future.
+        rotors = (
+            Walzen(wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', notch='Q'),
+            Walzen(wiring='AJDKSIRUXBLHWTMCQGZNPYFVOE', notch='E'),
+            Walzen(wiring='BDFHJLCPRTXVZNYEIWGAKMUSQO', notch='V'),
+        )[::-1]
+
+        reflector = Umkehrwalze(wiring='YRUHQSLDPXNGOKMIEBFZCWVJAT')
+
+        self.machine = Enigma(rotors=rotors, reflector=reflector)
 
 
 def run_tests():
