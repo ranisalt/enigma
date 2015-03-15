1a5aeabcdfae02125e167e8a221de4151819f5b5
Ranieri Althoff <ranisalt@gmail.com>
2015-03-15 00:59:11 -0300

    Test if default rotor encodes backwards properly

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ class RotorTestCase(unittest.TestCase):
         self.assertEqual('E', rotor.encode('A'))
 
 
+    def test_rotor_reverse_encoding(self):
+        rotor = Walzen(wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', notch='Q')
+        self.assertEqual('U', rotor.encode_reverse('A'))
+
+
 def run_tests():
     runner = unittest.TextTestRunner()
     suite = unittest.TestLoader().loadTestsFromTestCase(RotorTestCase)
