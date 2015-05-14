91587719320682b8550ce14cda22e804f67c79a4
Ranieri Althoff <ranisalt@gmail.com>
2015-05-14 04:01:38 -0300

    Major refactoring of code
    Changes related to commit c70900f29a4266ba7fa6f9ccaf3f96536e25673d
    API NOT compatible with previous commits

--- enigma.py
+++ enigma.py
@@ -1,85 +1,63 @@
 import string
 
+alphabet = string.ascii_uppercase
+
 
 class Steckerbrett:
     def __init__(self, *args):
-        self.map = {}
+        map = {}
 
         for arg in args:
-            if arg[0] in self.map or arg[1] in self.map:
+            if arg[0] in map or arg[1] in map:
                 raise KeyError('Same letter used twice in plugboard')
 
-            self.map[arg[0]] = arg[1]
-            self.map[arg[1]] = arg[0]
+            map[arg[0]] = arg[1]
+            map[arg[1]] = arg[0]
 
-    def swap(self, letter):
-        if letter in self.map:
-            return self.map[letter]
-        return letter
+        self.swap = lambda letter: map[letter] if letter in map else letter
 
 
 class Umkehrwalze:
     def __init__(self, wiring):
         assert isinstance(wiring, str)
-        if (sorted(wiring) != list(string.ascii_uppercase)):
+        if (sorted(wiring) != list(alphabet)):
             raise KeyError('Plugboard should contain every letter only once')
 
-        self.wiring = wiring
-
-    def encode(self, letter):
-        return self.wiring[string.ascii_uppercase.index(letter)]
+        self.encode = lambda letter: wiring[alphabet.index(letter)]
 
 
 class Walzen:
-    def __init__(self, notch, wiring, ringstellung='A', offset='A'):
-        assert isinstance(notch, str)
-        assert isinstance(wiring, str)
-        assert len(wiring) == len(string.ascii_uppercase)
+    def __init__(self, notch, wiring, ringstellung='A'):
+        self.notch = alphabet.index(notch)  # n
+        self.ringstellung = alphabet.index(ringstellung)  # r
+        self.perm = str.maketrans(string.ascii_uppercase, wiring)
+        self.rev_perm = str.maketrans(wiring, string.ascii_uppercase)
 
-        self.notch = notch
+        self._shift_up = lambda letter, j: alphabet[
+            (alphabet.index(letter) + j) % 26]
+        self._shift_down = lambda letter, j: alphabet[
+            (alphabet.index(letter) - j) % 26]
 
-        if isinstance(ringstellung, str) and len(ringstellung) == 1:
-            self.ringstellung = string.ascii_uppercase.index(ringstellung)
-        elif isinstance(ringstellung, int) and 0 <= ringstellung < len(wiring):
-            self.ringstellung = ringstellung
-        else:
-            raise ValueError('setting must be character or integer')
+    def encode(self, letter, j=0):
+        return self._shift_down(
+            str.translate(
+                self._shift_up(letter, j),
+                self.perm),
+            j)
 
-        if isinstance(offset, str) and len(offset) == 1:
-            self.offset = string.ascii_uppercase.index(offset)
-        elif isinstance(offset, int) and 0 <= offset < len(wiring):
-            self.offset = offset
-        else:
-            raise ValueError('offset must be character or integer')
-
-        self.wiring = wiring
-
-    def encode(self, letter):
-        index = string.ascii_uppercase.index(letter) - self.ringstellung
-        letter = self.wiring[index % len(self.wiring)]
-
-        index = string.ascii_uppercase.index(letter) + self.ringstellung - self.offset
-        letter = string.ascii_uppercase[index % len(self.wiring)]
-
-        return letter
-
-    def encode_reverse(self, letter):
-        index = string.ascii_uppercase.index(letter) - self.ringstellung + self.offset
-        letter = string.ascii_uppercase[index % len(self.wiring)]
-
-        index = self.wiring.index(letter) + self.ringstellung
-        letter = string.ascii_uppercase[index % len(self.wiring)]
-
-        return letter
-
-    def advance(self):
-        self.ringstellung = (self.ringstellung - 1) % len(self.wiring)
+    def encode_reverse(self, letter, j=0):
+        return self._shift_down(
+            str.translate(
+                self._shift_up(letter, j),
+                self.rev_perm),
+            j)
 
 
 class Enigma:
-    def __init__(self, rotors, reflector, plugboard=None):
+    def __init__(self, rotors, reflector, plugboard=Steckerbrett(),
+                 grundstellung='AAA'):
         # Assert that plugboard is a Steckerbrett if not None
-        assert plugboard is None or isinstance(plugboard, Steckerbrett)
+        assert isinstance(plugboard, Steckerbrett)
 
         # Assert that rotors is a tuple and each tuple element is a Walzen
         assert isinstance(rotors, tuple)
@@ -92,6 +70,7 @@ class Enigma:
         self.plugboard = plugboard
         self.rotors = rotors
         self.reflector = reflector
+        self.grundstellung = tuple(alphabet.index(l) for l in grundstellung)
 
     def cipher(self, message):
         assert isinstance(message, str)
@@ -99,38 +78,36 @@ class Enigma:
         message = message.upper()
         ciphered = ''
 
-        for letter in message:
+        # pode pegar esses valores no __init__ também
+        # essas funções lambda são puro charme.
+        p = lambda n: self.grundstellung[n]
+        r = lambda n: self.rotors[n].ringstellung
+        n = lambda n: self.rotors[n].notch
+        encode = lambda i, letter, exp: self.rotors[i].encode(letter, exp)
+        rencode = lambda i, letter, exp: self.rotors[i].encode_reverse(letter,
+                                                                       exp)
+
+        m1 = (n(0) - p(0)) % 26
+        m2 = m1 + 26 * ((n(1) - p(1) - 1) % 26) + 1
+
+        i1 = p(0) - r(0) + 1
+
+        for j, letter in enumerate(message):
             if letter in string.ascii_uppercase:
-                self._rotate()
+                # usando int() pra arredondar
+                k1, k2 = int((j - m1 + 26) / 26), int((j - m2 + 650) / 650)
+                i2, i3 = p(1) - r(1) + k1 + k2, p(2) - r(2) + k2
 
-                if self.plugboard is not None:
-                    letter = self.plugboard.swap(letter)
-
-                for rotor in self.rotors:
-                    letter = rotor.encode(letter)
-
+                letter = self.plugboard.swap(letter)
+                letter = encode(2, encode(1, encode(0, letter, i1 + j), i2), i3)
                 letter = self.reflector.encode(letter)
-
-                for rotor in self.rotors[::-1]:
-                    letter = rotor.encode_reverse(letter)
-
-                if self.plugboard is not None:
-                    letter = self.plugboard.swap(letter)
+                letter = rencode(0, rencode(1, rencode(2, letter, i3), i2),
+                                 i1 + j)
+                letter = self.plugboard.swap(letter)
 
             ciphered += letter
 
         return ciphered
-
-    def _rotate(self):
-        self.rotors[0].advance()
-
-        for index in range(len(self.rotors) - 1):
-            rotor = self.rotors[index]
-            if rotor.notch == rotor.wiring[rotor.ringstellung]:
-                if index == 1:
-                    rotor.advance()
-
-                self.rotors[index + 1].advance()
 
 
 if __name__ == '__main__':
